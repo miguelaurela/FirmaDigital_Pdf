e06f020b4ba0b462fbcdd6de3bf29bccc8621e04
Miguel Aurela <miguelaurela2804@gmail.com>
2023-05-11 01:30:57 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using FirmaDigital;
+using FirmaPDF;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+
+            }
+        }
+    }
+}
